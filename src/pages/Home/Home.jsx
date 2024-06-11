c9310267521017019408f14fcd52258711ee2584
AnnaHtslk <hannah.htslk@gmail.com>
2024-06-11 20:23:41 +0300

    added not found

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+import s from './Home.module.css';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>
+        Need a car? <br /> Go get it
+      </h1>
+      <Link to="/catalog">
+        <button type="button" className={s.button}>
+          Rent a car
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+import s from './NotFound.module.css';
 const NotFound = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className={s.wrapper}>
+      <h2 className={s.title}>Page not found</h2>
+      <Link className={s.link} to="/">
+        Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
