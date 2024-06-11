e2427cf7d7c85f8dbc68d8a7ac134cd6fff1c6e5
AnnaHtslk <hannah.htslk@gmail.com>
2024-06-11 19:29:22 +0300

    styled header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import s from './NavBar.module.css';
+import clsx from 'clsx';
 
 const NavBar = () => {
+  const activeClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
   return (
     <header className={s.header}>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/catalog">Catalog</NavLink>
-        <NavLink to="/favorites">Favorites</NavLink>
+      <nav className={s.nav}>
+        <NavLink className={activeClass} to="/">
+          Home
+        </NavLink>
+        <NavLink className={activeClass} to="/catalog">
+          Catalog
+        </NavLink>
+        <NavLink className={activeClass} to="/favorites">
+          Favorites
+        </NavLink>
       </nav>
     </header>
   );
