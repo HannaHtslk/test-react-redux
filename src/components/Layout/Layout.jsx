ddbb9afee310622ae9df8046fd1a0bdbcfd7aa1f
AnnaHtslk <hannah.htslk@gmail.com>
2024-06-11 18:02:02 +0300

    added Layout and Navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import s from './Layout.module.css';
+import NavBar from '../NavBar/NavBar';
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <>
+      <NavBar />
+      <div className={s.outletWrapper}>
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,16 @@
+import { NavLink } from 'react-router-dom';
+import s from './NavBar.module.css';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className={s.header}>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/catalog">Catalog</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
