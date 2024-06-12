05b787071c03251840d106887d632db5c13e5d9e
AnnaHtslk <hannah.htslk@gmail.com>
2024-06-12 19:16:58 +0300

    upd card list

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -7,33 +7,37 @@ const CarItem = () => {
       <img className={s.img} src="../../../public/bg.webp" alt="testPhoto" />
       <div className={s.content}>
         <div className={s.model}>
-          <p className={s.title}>BMW, 2009</p> <p className={s.title}>$50</p>
+          <p className={s.title}>BMW, 2009</p>
+          <p className={s.title}>$50</p>
         </div>
         <div className={s.details}>
-          <ul className={s.list}>
-            <li className={s.item}>
-              <p className={s.feature}>Kharkiv</p>
-            </li>
-            <li className={s.item}>
-              <p className={s.feature}>Ukraine</p>
-            </li>
-            <li className={s.item}>
-              <p className={s.feature}>Elite Car Rentals</p>
-            </li>
-
-            <li className={s.item}>
-              <p className={s.feature}>Premium SUV</p>
-            </li>
-            <li className={s.item}>
-              <p className={s.feature}>Navigator L</p>
-            </li>
-            <li className={s.item}>
-              <p className={s.feature}>9596</p>
-            </li>
-            <li className={s.item}>
-              <p className={s.feature}>Leather seats</p>
-            </li>
-          </ul>
+          <div>
+            <ul className={s.list}>
+              <li className={s.item}>
+                <p className={s.feature}>Kharkiv</p>
+              </li>
+              <li className={s.item}>
+                <p className={s.feature}>Ukraine</p>
+              </li>
+              <li className={s.item}>
+                <p className={s.feature}>Elite Car Rentals</p>
+              </li>
+            </ul>
+            <ul className={s.list}>
+              <li className={s.item}>
+                <p className={s.feature}>SUV</p>
+              </li>
+              <li className={s.item}>
+                <p className={s.feature}>Navigator L</p>
+              </li>
+              <li className={s.item}>
+                <p className={s.feature}>9596</p>
+              </li>
+              <li className={s.item}>
+                <p className={s.feature}>Leather seats</p>
+              </li>
+            </ul>
+          </div>
           <Button>Learn more</Button>
         </div>
       </div>
