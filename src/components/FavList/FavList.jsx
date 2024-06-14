bcaa063a5b0b620423f9702d31aad230bbae9f2d
AnnaHtslk <hannah.htslk@gmail.com>
2024-06-14 16:25:36 +0300

    Update FavList.jsx

--- src/components/FavList/FavList.jsx
+++ src/components/FavList/FavList.jsx
@@ -1,23 +1,17 @@
-import { Link } from 'react-router-dom';
 import FavItem from '../FavItem/FavItem';
 import s from './FavList.module.css';
 
 const FavList = ({ favorites }) => {
   return (
-    <div className={s.wrapper}>
-      <Link className={s.link} to="/catalog">
-        back to catalog
-      </Link>
-      <ul className={s.list}>
-        {favorites.map(car => {
-          return (
-            <li className={s.item} key={car.id}>
-              <FavItem fav={car} />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul className={s.list}>
+      {favorites.map(car => {
+        return (
+          <li className={s.item} key={car.id}>
+            <FavItem fav={car} />
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
